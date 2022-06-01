9080597ed23eb1e368237ed25d7e64c00c05f9ea
Kenneth D. Mankoff <mankoff@gmail.com>
2022-06-01 15:04:01 -0700

    Try adding large NC a different way

--- cryo-data/download.sh
+++ cryo-data/download.sh
@@ -2,7 +2,11 @@
 
 # can be more involved, e.g. wrapping download-url in a wget spyder
 # See also https://docs.datalad.org/en/stable/generated/man/datalad-addurls.html
-datalad download-url https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IDBMG4.004/1993.01.01/BedMachineGreenland-2021-04-20.nc
+
+# download-url seems to fail on this very large
+# datalad download-url https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IDBMG4.004/1993.01.01/BedMachineGreenland-2021-04-20.nc
+
+printf "url,file\nhttps://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IDBMG4.004/1993.01.01/BedMachineGreenland-2021-04-20.nc,BedMachineGreenland-2021-04-20.nc\n" | datalad addurls --nosave --fast -t csv - '{url}' '{file}'
 
 datalad download-url https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IDBMG4.004/1993.01.01/BedMachineGreenland-2021-04-20.nc.xml
 
